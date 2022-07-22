ec251c5b62238f63568114a34ced6709efe107e0
pedramaghasian <pedram.aghasian@yahoo.com>
2022-07-22 11:47:47 +0430

    feat: config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('NESTJS_AUTHENTICATION')
+    .addBearerAuth()
+    .setVersion('0.0.1')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
