a88b660c7a9aae4c4489f8c844e2f4613f4911b6
pedramaghasian <pedram.aghasian@yahoo.com>
2022-07-22 12:12:50 +0430

    feat: user & auth module + mongoose config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const environment = process.env.NODE_ENV || 'development';
 @Module({
-  imports: [],
+  imports: [
+    UserModule,
+    AuthModule,
+
+    ConfigModule.forRoot({
+      envFilePath: `.env.${environment}`,
+      isGlobal: true,
+    }),
+
+    MongooseModule.forRoot(process.env.MONGODB_WRITE_CONNECTION_STRING, {
+      userNewUrlParser: true,
+      userUnifiedTopology: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
